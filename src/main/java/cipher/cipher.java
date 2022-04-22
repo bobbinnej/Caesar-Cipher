3e06a40a4c9f95df601c4cc4202f2bb0a0a8cb03
jackie <bobbinnej@gmail.com>
2022-04-22 16:20:32 +0300

    Create the .ignore file and add the files we want to be ignored into it

--- src/main/java/cipher/cipher.java
+++ src/main/java/cipher/cipher.java
@@ -1,4 +1,17 @@
 package cipher;
+// Import for scanner
+import java.util.Scanner;
 
 public class cipher {
+    public static void main(String[] args) {
+        //Declare your variables
+        String message;
+        int shiftKey;
+        // Begin with gathering user input using scanner
+        Scanner sc = new Scanner(System.in);
+        // Expected input is like
+        System.out.print("Please input the message to encrypt:\n");
+        message= sc.nextLine();
+
+    }
 }
