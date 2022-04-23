83f55beac3004c119c632f432fdc4773fc9207c5
jackie <bobbinnej@gmail.com>
2022-04-23 17:13:10 +0300

    Code business logic for the cipher application

--- src/main/java/cipher/cipher.java
+++ src/main/java/cipher/cipher.java
@@ -9,9 +9,21 @@ public class cipher {
         int shiftKey;
         // Begin with gathering user input using scanner
         Scanner sc = new Scanner(System.in);
-        // Expected input is like
+        /* Expected input is like
+        String message = "HELLO WORLD";
+         */
         System.out.print("Please input the message to encrypt:\n");
+        //.nextLine(); is used with strings
         message= sc.nextLine();
+
+          /* Expected input is like
+        int shiftKey = "-14";
+         */
+        System.out.println("Input desired shift key ( number between 1 and 25:)");
+        //.nextInt(); is used with integers
+        shiftKey=sc.nextInt();
+
+
 
     }
 }
