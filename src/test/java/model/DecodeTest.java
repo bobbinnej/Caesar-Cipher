0a2c288843b173da40b2cea90bffd0fe27bb2ba0
jackie <bobbinnej@gmail.com>
2022-04-24 17:32:18 +0300

    Perform test on Decode class

--- src/test/java/model/DecodeTest.java
+++ src/test/java/model/DecodeTest.java
@@ -1,7 +1,42 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecodeTest {
+    Decode testDecode= new Decode(2, "HELLO");
+    @Test
+    public void checkIfCreatesInstanceCorrectly(){
+        assertEquals(true, testDecode instanceof Decode);
+
+    }
+    @Test
+    public void checkIfMessageIsSaved(){
+        assertEquals("HELLO", testDecode.getMessage());
+    }
+    @Test
+    public void checkIfShiftKeyIsSaved(){
+        assertEquals(2, testDecode.getKey());
+    }
+    @Test
+    public void doesNotAcceptNegativeShiftKey(){
+        Encode testEncode = new Encode("HELLO", 2);
+        assertEquals("FCIIM", testDecode.decrypt());
+    }
+    @Test
+    public void checkIfIsValidKey(){
+        assertEquals(true, testDecode.sValidKey());
+    }
+
+    @Test
+    public void checkIfIsNotEmpty(){
+        assertEquals(true, testDecode.isNotEmpty());
+    }
+
+    @Test
+    public void checkIfIsValidMessage(){
+        assertEquals(true, testDecode.sValidMessage());
+    }
 
 }
